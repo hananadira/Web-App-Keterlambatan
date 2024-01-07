017cda5e102a31486208117eb0e07054bcda6555
hananadirsav <hananadira140107@gmail.com>
2024-01-08 06:52:16 +0700

    revisi tanggal dashboard PS

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -242,7 +242,7 @@ a.sidebar-link {
                     </li>
                     @else 
                     <li class="sidebar-item">
-                        <a href="{{ route('dashboardps') }}" class="sidebar-link">
+                        <a href="{{ route('dashboardps') }}" class="sidebar-link" onclick="setTanggal()">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
                                 <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5z"/>
                             </svg>
